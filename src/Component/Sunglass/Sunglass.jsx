13699a82ddf5335c90163e9a458c59515c5fd16c
imrulhassanemon <imrulhassan989@gmail.com>
2024-10-21 17:05:05 +0600

    Es6 Modul Overview

---
+++ src/Component/Sunglass/Sunglass.jsx
@@ -0,0 +1,19 @@
+import add from "../../Utils/calculate";
+
+
+const Sunglass = () => {
+    
+    const first = 20;
+    const second = 20;
+    const sum = add(first, second)
+    console.log(sum);
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Sunglass;
